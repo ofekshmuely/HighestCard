f8455095137a552e2f45146974cd6ae4d6b2aac8
Ofek <32801785+ofekshmuely@users.noreply.github.com>
2018-11-25 14:18:03 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+
+//2 random values
+let RandomA = Math.floor(Math.random() * 6) + 1; //setting up random for image1
+let RandomB = Math.floor(Math.random() * 6) + 1; //setting up random for image2
+
+
+
+/* ----- A and B images changing value ----- */
+let randomimageA = "images/dice" + RandomA + ".png";
+let randomimageB = "images/dice" + RandomB + ".png";
+
+
+
+document.querySelectorAll("img")[0].setAttribute("src", randomimageA);
+document.querySelectorAll("img")[1].setAttribute("src", randomimageB); 
+
+
+
+//terms of h1 headline
+    if (RandomA > RandomB) {
+        document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+        document.querySelector("p1wins").innerHTML = "Test!";
+
+      }
+      else if (RandomB > RandomA) {
+        document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+      }
+      else {
+        document.querySelector("h1").innerHTML = "Its a Draw!";
+      }
+      
+    //play again
+      function refreshPage(){
+        window.location.reload();
+    } 
+
+
